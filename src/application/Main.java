de775c97fe3724d6739ed33848c8793ad8dbda1e
meghellet <said199811@live.fr>
2022-04-14 18:52:06 +0200

    premier commit [Test]

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,9 +11,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,500,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Interface Utilisateur");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
