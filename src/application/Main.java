828abb650e8da216218b2470bd6cc29e174ebe65
meghellet <said199811@live.fr>
2022-04-16 12:27:43 +0200

    Creation de la fenetre profil

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,20 +1,32 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		try {
+		try {/*
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,500,400);
+			Scene scene = new Scene(root,1000,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("Interface Utilisateur");
+			primaryStage.setTitle("Profil");
+			primaryStage.show();
+			*/
+			//AnchorPane root = (AnchorPane) FXMLLoader.load(Main.class.getResource("/application/Profil.fxml"));
+			Parent root  = FXMLLoader.load(getClass().getResource("/application/Profil.fxml"));
+			Scene scene = new Scene(root,1000,600);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Profil");
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
---
+++ src/application/ProfilController.java
@@ -0,0 +1,52 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class ProfilController {
+	 	@FXML
+	    private TextField Adresse;
+
+	    @FXML
+	    private TextField Age;
+
+	    @FXML
+	    private TextField Email;
+
+	    @FXML
+	    private ImageView ImageProfil;
+
+	    @FXML
+	    private Button Modifier;
+	    
+	    @FXML
+	    private Button Changer;
+
+
+	    @FXML
+	    private TextField Nom;
+
+	    @FXML
+	    private TextField Num;
+
+	    @FXML
+	    private TextField Prenom;
+
+	    @FXML
+	    private ComboBox<?> Sexe;
+
+	    @FXML
+	    void ModifierProfil(ActionEvent event) {
+
+	    }
+	    @FXML
+	    void ChangerPhotoProfil(ActionEvent event) {
+
+	    }
+
+
+}
